e21ab6045b6af8eff54e6c3f7c86b9413279a00c
f24ba035 <f24ba035@chuo.ac.jp>
2025-06-04 16:25:39 +0900

    feat

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Generated App',
-      theme: ThemeData(
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
-        primaryColor: const Color(0xff2196f3),
-        canvasColor: const Color(0xffafafa),
+        primaryColor: const Color(0xFF2196f3),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blue,
+        ).copyWith(secondary: const Color(0xFF2196f3)),
+        canvasColor: const Color(0xFFfafafa),
       ),
-      home: MyHomePage(),
+      home: new MyHomePage(),
     );
   }
 }
@@ -22,27 +25,64 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key}) : super(key: key);
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static var _message = 'ok.';
+  static var _checked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('App Name')),
-      body: Container(
-        child: Text(
-          "Hello Flutter!",
-          style: TextStyle(
-            fontSize: 32.0,
-            color: const Color(0xff000000),
-            fontWeight: FontWeight.w700,
-            fontFamily: "Roboto",
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                _message,
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Roboto",
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Switch(value: _checked, onChanged: checkChanged),
+
+                  Text(
+                    "Checkbox",
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "Roboto",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        padding: const EdgeInsets.all(10.0),
-        alignment: Alignment.bottomCenter,
       ),
     );
+  }
+
+  void checkChanged(bool? value) {
+    setState(() {
+      _checked = value!;
+      _message = value ? 'checked!' : 'not checked...';
+    });
   }
 }
--- 授業まとめ/flutter.studio.md
+++ 授業まとめ/flutter.studio.md
@@ -2,9 +2,9 @@
 
 ウィジェット作成についてまとめています。
 
-## flutterstudio
+# flutterstudio
 
-### Center
+## Center
 ```body: Center()```の中に真ん中に配置したい内容を書くと真ん中に文字が出る。
 ### Container
 ```body:Container()```の中に真ん中に配置したい内容を書くと下の中央に配置される。
@@ -17,4 +17,36 @@ EdgeInsetsを使うことで、配置する位置も決めることができる�
 の中に```<widget>[リスト...]```を使ってリスト内にウィジェットを配置すると、リスト内のウィジェットが順番に横並びに配置される。
 
 ### textbButton
+```children: <Widget>[]```のカッコ内に書くことができる。
+テキストをボタンとして表示できるが、平面的。
+
+### ElevatedButton
+``` children: <Widget>[]```の中に書くことができる。
+立体的なボタンが表示される。
+
+### IconButton
+``` children: <Widget>[]```のなかに書くことができる。
+アイコンをボタンとしてひょうじできる。
+
+## Bottonウィジェットについて、
+```textbButton```,```ElevatedButton```,```IconButton```はボタンを付けるためのウィジェットです。
+
+### TextField
+```class _MyhomepageState extends State<MyHomePage>```で宣言する
+ボタンを押すとテキストが表示される。
+
+### onChanged
+```class _MyhomepageState extends State<MyHomePage>```で宣言する。
+テキストを入力できる。入力した文字をリアルタイムで入力値に処理を加えることができる。
+
+### Checkbox
+```class _MyhomepageState extends State<MyHomePage>```で宣言して、
+チェックボックスを表示することができる。
+### Switch
+```children: <Widget>[]```の中に書くことで、オンオフswitchを追加することができる。
+### Radio
+```children: <Widget>[]```の中に書くことで、ラジオボタンを追加することができる。
+
+
+
 
